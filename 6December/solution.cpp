9fac7572f1a873c58098d333017ba6f97f558043
Andreas <Andreas.ravnholt99@gmail.com>
2024-12-10 12:01:28 +0100

    Solution for part 2 of advent of code

--- 6December/solution.cpp
+++ 6December/solution.cpp
@@ -117,3 +117,129 @@ int solution6(){
 
     return result;
 }
+
+//Second solution
+
+using Position = std::pair<size_t, size_t>;
+
+bool check_loop(std::vector<std::string> data, Position player, Position obstacle){
+    size_t y = player.first;
+    size_t x = player.second;
+    std::set<std::tuple<Position, char>> visited;
+    data[obstacle.first][obstacle.second] = 'O';
+    std::tuple<Position, char> vis;
+    const size_t STEP_LIMIT = 1e6;
+    size_t steps = 0;
+
+    while(y < data.size() && x < data[0].size()){
+        steps++;
+        if(steps > STEP_LIMIT) return true;
+        if(data[y][x] == '^'){
+            data[y][x] = 'X'; 
+            vis = {{y, x}, '^'};
+            if(y-1 < 0 || y == 0) break;
+            if(data[y-1][x] == 'O' && visited.count(vis) > 0) return true;
+            visited.insert(vis);
+            if((data[y-1][x] == '#' || data[y-1][x] == 'O') && x+1 < data[0].size() && (data[y][x+1] == '#' || data[y][x+1] == 'O')){
+                y++;
+                data[y][x] = 'v';
+            }
+            else if(data[y-1][x] == '#' || data[y-1][x] == 'O'){
+               x++;
+               data[y][x] = '>';
+            } 
+            else{
+               y--;
+               data[y][x] = '^'; 
+            }
+        } 
+        else if(data[y][x] == '>'){
+            data[y][x] = 'X'; 
+            vis = {{y, x}, '>'};
+            if(x+1 >= data[0].size() || x == data[0].size()) break;
+            if(data[y][x+1] == 'O' && visited.count(vis) > 0) return true;
+            visited.insert(vis);
+            if((data[y][x+1] == '#' || data[y][x+1] == 'O') && y+1 < data.size() && (data[y+1][x] == '#' || data[y+1][x] == 'O')){
+                x--;
+                data[y][x] = '<';
+            }
+            else if(data[y][x+1] == '#' || data[y][x+1] == 'O'){
+               y++;
+               data[y][x] = 'v';
+            } 
+            else{
+               x++;
+               data[y][x] = '>'; 
+            }
+        } 
+        else if(data[y][x] == 'v'){
+            data[y][x] = 'X'; 
+            vis = {{y, x}, 'v'};
+            if(y+1 >= data.size() || y == data.size()) break;
+            if(data[y+1][x] == 'O' && visited.count(vis) > 0) return true;
+            visited.insert(vis);
+            if((data[y+1][x] == '#' || data[y+1][x] == 'O') && x-1 >= 0 && (data[y][x-1] == '#' || data[y][x-1] == 'O')){
+                y--;
+                data[y][x] = '^';
+            }
+            else if(data[y+1][x] == '#' || data[y+1][x] == 'O'){
+               x--;
+               data[y][x] = '<';
+            } 
+            else{
+               y++;
+               data[y][x] = 'v'; 
+            }
+        } 
+        else if(data[y][x] == '<'){
+            data[y][x] = 'X'; 
+            vis = {{y, x}, '<'};
+            if(x-1 < 0 || x == 0) break;
+            if(data[y][x-1] == 'O' && visited.count(vis) > 0) return true;
+            visited.insert(vis);
+            if((data[y][x-1] == '#' || data[y][x-1] == 'O') && y-1 >= 0 && (data[y-1][x] == '#' || data[y-1][x] == 'O')){
+                x++;
+                data[y][x] = '>';
+            }
+            else if(data[y][x-1] == '#' || data[y][x-1] == 'O'){
+               y--;
+               data[y][x] = '^';
+            } 
+            else{
+               x--;
+               data[y][x] = '<'; 
+            }
+        } 
+    }
+
+    return false;
+}
+
+int second_solution6(){
+    int result = 0;
+    std::vector<std::string> data = get_data6();
+    Position player;
+    for(size_t i = 0; i < data.size(); i++){
+        for(size_t j = 0; j < data[0].size(); j++){
+            if(data[i][j] == '^' || data[i][j] == 'v' || data[i][j] == '<' || data[i][j] == '>'){
+                player.first = i;
+                player.second = j;
+                i = data.size() + 1;
+                j = data[0].size() + 1;
+            }
+        }    
+    }
+
+    for(size_t i = 0; i < data.size(); i++){
+        for(size_t j = 0; j < data[0].size(); j++){
+            Position obstacle = {i, j};
+            if(obstacle == player) continue;
+            if(check_loop(data, player, obstacle)){
+                result++;
+            }
+        }    
+    }
+
+    return result;
+}
+
