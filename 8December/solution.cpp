15186df951d9eedee4225b4632edf4bed72490a7
Andreas <Andreas.ravnholt99@gmail.com>
2024-12-12 15:24:34 +0100

    Solution for Part 2 of 8th of december

--- 8December/solution.cpp
+++ 8December/solution.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <vector>
 
 //First solution
 std::vector<std::string> get_data8(){
@@ -20,18 +19,6 @@ std::vector<std::string> get_data8(){
 }
 
 size_t solution8(){
-    //std::vector<std::string> data = {"............",
-    //                                 "........0...",
-    //                                 ".....0......",
-    //                                 ".......0....",
-    //                                 "....0.......",
-    //                                 "......A.....",
-    //                                 "............",
-    //                                 "............",
-    //                                 "........A...",
-    //                                 ".........A..",
-    //                                 "............",
-    //                                 "............"};
     std::vector<std::string> data = get_data8();
     std::map<std::pair<size_t, size_t>, char> node_positions;
 
@@ -57,6 +44,48 @@ size_t solution8(){
             else y = itr.first.first - (second_itr.first.first - itr.first.first);
             if(itr.first.second > second_itr.first.second) x = itr.first.second + (itr.first.second - second_itr.first.second); 
             else x = itr.first.second - (second_itr.first.second - itr.first.second);
+            if(y >= 0 && y < data.size() && x >= 0 && x < data[0].size()) already_counted.insert({y, x});
+        }
+    }
+    
+    return already_counted.size();
+}
+
+//Second Solution
+size_t second_solution8(){
+    std::vector<std::string> data = get_data8();
+    std::map<std::pair<size_t, size_t>, char> node_positions;
+
+    for(size_t i = 0; i < data.size(); i++){
+        for(size_t j = 0; j < data[i].size(); j++){
+           if(data[i][j] != '.') {
+               std::pair<size_t, size_t> current;
+               current.first = i;
+               current.second = j;
+               node_positions[current] = data[i][j];
+           }
+        }
+    }
+
+    std::map<std::pair<size_t, size_t>, char>::iterator itr;
+    std::set<std::pair<size_t, size_t>> already_counted;
+    for(auto const& itr : node_positions){
+        already_counted.insert(itr.first);
+        for(auto const& second_itr : node_positions){
+            if(itr == second_itr || itr.second != second_itr.second) continue;
+            size_t y;
+            size_t x;
+            if(itr.first.first > second_itr.first.first) y = itr.first.first + (itr.first.first - second_itr.first.first); 
+            else y = itr.first.first - (second_itr.first.first - itr.first.first);
+            if(itr.first.second > second_itr.first.second) x = itr.first.second + (itr.first.second - second_itr.first.second); 
+            else x = itr.first.second - (second_itr.first.second - itr.first.second);
+            while(y >= 0 && y < data.size() && x >= 0 && x < data[0].size()){
+                already_counted.insert({y, x});
+                if(itr.first.first > second_itr.first.first) y += itr.first.first - second_itr.first.first; 
+                else y -= second_itr.first.first - itr.first.first;
+                if(itr.first.second > second_itr.first.second) x += itr.first.second - second_itr.first.second; 
+                else x -= second_itr.first.second - itr.first.second;
+            }
             if(y >= 0 && y < data.size() && x >= 0 && x < data[0].size()) already_counted.insert({y, x});
         }
     }
--- 8December/solution.h
+++ 8December/solution.h
@@ -1,5 +1,6 @@
 #ifndef SOLUTION8_H
 #define SOLUTION8_H
+#include <bits/stdc++.h>
 size_t solution8();
-int second_solution8();
+size_t second_solution8();
 #endif
