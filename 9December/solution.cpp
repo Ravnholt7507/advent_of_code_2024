f3227dbe3a07b77930e5d379ea0f92ba67581638
Andreas <Andreas.ravnholt99@gmail.com>
2024-12-13 09:33:34 +0100

    Solution for part one of 9th of december

--- 9December/solution.cpp
+++ 9December/solution.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <string>
 
 //First Solution
 std::string get_data9(){
@@ -23,37 +24,42 @@ uint64_t solution9(){
     uint64_t result = 0;
     //std::string data = "2333133121414131402";
     std::string data = get_data9();
-    std::string converted_data = "";
+    std::vector<std::string> converted_data{};
     
     bool block = true;
     int index = 0;
     for(char c : data){
         if(block){
             for(int i = 0; i < c - '0'; i++){
-                converted_data += std::to_string(index);
+                converted_data.push_back(std::to_string(index));
             }
             index++;
             block = false;
         } else {
             for(int i = 0; i < c - '0'; i++){
-                converted_data += '.';
+                converted_data.push_back(".");
             }
             block = true;
         }
     }
 
-    for(size_t i = converted_data.size() - 1; i >= 0; i-- ){
-        if(converted_data[i] == '.') continue;
-        int j = converted_data.find('.');
-        if(converted_data.find('.') >= i) break;
-        converted_data[j] = converted_data[i];
-        converted_data[i] = '.';
+    for(size_t i = converted_data.size() - 1; i > 0; i-- ){
+        if(converted_data[i] == ".") continue;
+        size_t index; 
+        for(size_t j = 0; j < converted_data.size(); j++){
+            if(converted_data[j] == "."){
+                index = j;
+                break;
+            }
+        }
+        if(index >= i) break;
+        converted_data[index] = converted_data[i];
+        converted_data[i] = ".";
     }
-
     for(size_t i = 0; i < converted_data.size(); i++){
-        if(converted_data[i] == '.') break;
-        std::cout<< i * (converted_data[i] - '0') << std::endl;
-        result += i * (converted_data[i] - '0');
+        if(converted_data[i] == ".") break;
+        //std::cout<< i * (converted_data[i] - '0') << std::endl;
+        result += i * std::stoi(converted_data[i]);
     }
 
     return result;
